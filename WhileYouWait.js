0520b289240cace76f235ced95de5a9efe7e00ab
Jared <jaredjkim@gmail.com>
2017-11-12 19:31:38 -0800

    first commit

---
+++ WhileYouWait.js
@@ -0,0 +1,19 @@
+var num = 60;
+
+
+while (num = num - 1)
+{
+    if(num > 30)
+    {
+        console.log(num + " SUCH A LONG TIME!!!");
+    }
+    if(num <= 30 && num > 5)
+    {
+        console.log(num + " MY BIRTHDAY IS COMING SOON!!!")
+    }
+    if(num <= 5 && num > 0 )
+    {
+        console.log(num + " WAAAAHHHH MY BIRTHDAY IS COMING!!")
+    }
+  
+console.log( " ♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•* \n ♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪ \n *•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
